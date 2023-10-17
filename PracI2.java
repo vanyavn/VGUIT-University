1df878e57ab2eb85d63a16ee1df27e6b46bdef4b
vanyavn <114354433+vanyavn@users.noreply.github.com>
2023-10-17 12:21:31 +0300

    Update PracI2.java

--- PracI2.java
+++ PracI2.java
@@ -1,17 +1,1 @@
-import java.util.Scanner;
-public class PracI2 {
-   
-    public static void main(String[] args) {
-           
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите Ваше количество параметров: ");
-        String num = in.nextLine();
-        if (num.length() > 0){
-            System.out.printf("Вы ввели %s параметров\n", num);
-        }
-        else {
-            System.out.println("Вы не передавали параметров");
-        }   
-        in.close();
-    }
-}
+s
